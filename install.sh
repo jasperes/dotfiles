4a5b6802bf1cc6c3f4ac8a9f3586d9436ffd0ce5
Jonathan Peres <jonathan.peres@protonmail.com>
2026-08-11 22:38:09 -0300

    wip

--- install.sh
+++ install.sh
@@ -9,12 +9,13 @@ current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p "${HOME}/.config/mise/"
 mkdir -p "${HOME}/.config/mise/conf.d/"
 
-ln -s "${current_dir}/dotfiles" "${HOME}/.dotfiles" || true
-ln -s "${current_dir}/profiles/config.toml" "${HOME}/.config/mise/config.toml" || true
-ln -s "${current_dir}/profiles/conf.d/${SYSTEM}.toml" "${HOME}/.config/mise/conf.d/system.toml" || true
-ln -s "${current_dir}/profiles/conf.d/${THEME}.toml" "${HOME}/.config/mise/conf.d/theme.toml" || true
+# ln -s "${current_dir}/dotfiles" "${HOME}/.dotfiles" || true
+ln -sf "${HOME}/.dotfiles/.config/mise/config.toml" "${HOME}/.config/mise/config.toml" || true
+ln -sf "${HOME}/.dotfiles/.config/mise/conf.d/${SYSTEM}.toml" "${HOME}/.config/mise/conf.d/system.toml" || true
+ln -sf "${HOME}/.dotfiles/.config/mise/conf.d/${THEME}.toml" "${HOME}/.config/mise/conf.d/theme.toml" || true
 
 curl https://mise.run | sh
+eval "$(mise activate bash)"
 mise bootstrap --force-dotfiles
 
 echo "All done. Restart system!" && exit 0
